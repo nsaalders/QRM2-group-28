c3577306f6642873220b206a20df6d78435ced3f
nsaalders <n.s.aalders@student.vu.nl>
2026-09-11 17:01:02 +0200

    Add files via upload

---
+++ QRM2.R
@@ -0,0 +1,10 @@
+1. observations: nrow(movies1) * ncol(movies1)
+variables: nrow(movies1)
+subjects: ncol(movies1)
+
+2. nominal: first_actor_gender
+ordinal: not present
+interval: release_date
+ratio: budget
+
+3.
